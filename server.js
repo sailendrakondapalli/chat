03ffd7561c105620d8a46c5f0df0f6b31630751c
sailendrakondapalli <sailendrakondapalli@gmail.com>
2025-01-21 08:00:04 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -26,21 +25,26 @@ app.set('view engine', 'ejs');
 
 // Database connection
 mongoose.connect(process.env.url)
-  .then(() => console.log('yeah connected'))
-  .catch((err) => console.error(err));
+  .then(() => console.log('Database connected successfully'))
+  .catch((err) => console.error('Database connection error:', err));
 
 // Session store
 const store = new MongoDBStore({
   uri: process.env.url,
   collection: 'sessions',
 });
-
+ 
 app.use(
   session({
-    secret: 'secret',
+    secret: process.env.SESSION_SECRET || 'default_secret',
     resave: false,
     saveUninitialized: false,
     store: store,
+    // cookie: {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === 'production', // Ensure cookies are secure in production
+    //   maxAge: 1000 * 60 * 60 * 24, // 1 day
+    // },
   })
 );
 
@@ -83,6 +87,7 @@ app.post('/register', async (req, res) => {
     let user = await User.findOne({ email });
     if (user) {
       return res.redirect('/register');
+    //   return res.status(400).send('All fields are required');
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -94,13 +99,25 @@ app.post('/register', async (req, res) => {
     });
 
     await user.save();
-    res.redirect('/login');
+
+
+    req.session.isAuthenticated = true;
+    req.session.userId = user._id;
+    req.session.username = user.username;
+    res.redirect('/chat');
+    
   } catch (err) {
     console.error('Error registering user:', err);
     res.redirect('/register');
   }
 });
-
+app.post('/logout',(req,res)=>{
+    req.session.destroy((err)=>{
+        if(err)throw err;
+        res.redirect('/login')
+    })
+})
+  
 app.post('/user-login', async (req, res) => {
   const { email, password } = req.body;
 
